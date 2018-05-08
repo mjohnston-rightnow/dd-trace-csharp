0d4b71c1e5ecc807d6dab9287f37d6c3343a0763
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-05-08 16:38:38 -0400

    add FxCop analyzer and several temporary suppressions

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -8,3 +8,6 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Reviewed.")]
 [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:FieldNamesMustNotBeginWithUnderscore", Justification = "Reviewed.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Reviewed.")]
+
+// TODO: remove unused private fields and remove this suppression
+[assembly: SuppressMessage("FxCop.Performance", "CA1823:Avoid unused private fields", Justification = "Temporary.")]
--- test/GlobalSuppressions.cs
+++ test/GlobalSuppressions.cs
@@ -5,7 +5,12 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:Elements must be ordered by access", Justification = "Reviewed.")]
-
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:EnableXmlDocumentationOutput", Justification = "Reviewed.")]
+
+[assembly: SuppressMessage("FxCop.Reliability", "CA2007:Do not directly await a Task", Justification = "Reviewed.")]
+[assembly: SuppressMessage("FxCop.Naming", "CA1707:Identifiers should not contain underscores", Justification = "Reviewed.")]
+[assembly: SuppressMessage("FxCop.Performance", "CA1822:Mark members as static", Justification = "Reviewed.")]
+
+// TODO: fix this issues and remove this suppression
+[assembly: SuppressMessage("FxCop.Globalization", "CA1305:Specify IFormatProvider", Justification = "Temporary.")]
+[assembly: SuppressMessage("FxCop.Globalization", "CA1304:Specify CultureInfo", Justification = "Temporary.")]
